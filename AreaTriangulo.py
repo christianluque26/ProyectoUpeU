702918ab1190b3ce7579f8fd85f0f2f6a86abde2
christianluque3 <>
2021-06-09 22:23:30 +0000

    Areas de trinagulo

---
+++ AreaTriangulo.py
@@ -0,0 +1,9 @@
+#defenicion de variables y otros
+print("ejercicios 01 Area de un Triangulo ")
+#datos de entrada mediantes dispositivos de entrada
+b=int(input("ingrese base:"))
+h=int(input("ingrese altura:"))
+#proceso
+area=(b*h)/2
+#datos de salida en dispositivos de salida 
+print("Area de Triangulo es ", area)
---
+++ index.py
@@ -0,0 +1,7 @@
+print("Hola mundo")
+a=30
+b=5
+print("suma:",a+b)
+print("resta:",a+b)
+print("dividion:",a/b)
+print("multiplicacion:",a*b)
